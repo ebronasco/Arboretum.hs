03e37cdfc40292b57a0b1625787f4702b21fded4
Eugen <ebronasco@gmail.com>
2024-06-21 11:48:03 +0200

    Defined abstract syntax tree, abstract operations to go in its nodes, composition, and symmetric composition. Stopped at defining `eval`. Need to figure out where to used `a` and where `VectorBasis a`.

--- src/RootedTree.hs
+++ src/RootedTree.hs
@@ -26,11 +26,13 @@ module RootedTree (
     Graftable,
     graft,
     gl,
+    Operation (..),
+    graftOp,
+    concatOp,
     SyntacticTree (..),
-    Substitutable,
-    substitution,
-    substitute,
     syntacticToPlanar,
+    compose,
+    symmetricCompose,
 ) where
 
 import Data.List (intercalate, permutations)
@@ -223,8 +225,8 @@ instance (Ord d) => Planarable (MS.MultiSet (Tree d)) where
 
 -- * Grafting product
 
-class Graftable t where
-    graft :: t -> t -> Vector Integer t
+class (IsVector a) => Graftable a where
+    graft :: a -> a -> Vector (VectorScalar a) (VectorBasis a)
 
 {- | Grafting of two planar forests using the @deshuffleCoproduct@ function that splits @f1@ into subforests in all possible ways.
 
@@ -290,16 +292,23 @@ gl f1 f2 = linear perCoproductTerm $ deshuffleCoproduct f1
 
 -- * Substitution
 
-class IsSyntacticTree t where
-    type EvaluatesTo t
+data Operation a = Op
+    { name :: String
+    , tex :: String
+    , arity :: Int
+    , func :: [a] -> Vector (VectorScalar a) a
+    }
 
-    eval :: t -> Vector Integer (EvaluatesTo t)
-    syn :: EvaluatesTo t -> t
-    compose :: t -> [t] -> t -> Maybe t
+instance (Eq a) => Eq (Operation a) where
+    (Op n1 _ _ _) == (Op n2 _ _ _) = n1 == n2
 
-data Operation = Op {name :: String, argN :: Int, tex :: String} deriving (Eq)
-
-data SyntacticTree a = Node {operation :: Operation, arguments :: [SyntacticTree a]} | Leaf a deriving (Eq)
+data SyntacticTree a
+    = Node
+        { operation :: Operation a
+        , arguments :: [SyntacticTree a]
+        }
+    | Leaf a
+    deriving (Eq)
 
 instance (Graded a) => Graded (SyntacticTree a) where
     grading (Leaf a) = grading a
@@ -312,59 +321,61 @@ instance (Show a) => Show (SyntacticTree a) where
 instance (Show a, Texifiable a) => Texifiable (SyntacticTree a) where
     texify = texify . syntacticToPlanar
 
-syntacticToPlanar :: Show a => SyntacticTree a -> PlanarTree String
+syntacticToPlanar :: (Show a) => SyntacticTree a -> PlanarTree String
 syntacticToPlanar (Leaf a) = PT (show a) []
 syntacticToPlanar (Node op as) = PT (tex op) $ map syntacticToPlanar as
 
-instance (Eq a, Graded a) => IsVector (SyntacticTree a) where
-    type VectorScalar (SyntacticTree a) = Integer
+instance
+    ( IsVector a
+    , Eq (VectorScalar a)
+    , Num (VectorScalar a)
+    , Eq a
+    , Graded a
+    )
+    => IsVector (SyntacticTree a)
+    where
+    type VectorScalar (SyntacticTree a) = VectorScalar a
     type VectorBasis (SyntacticTree a) = SyntacticTree a
 
     vector = vector . (1 *^)
 
-instance (Eq a, Graded a) => IsSyntacticTree (SyntacticTree a) where
-    type EvaluatesTo (SyntacticTree a) = a
-
-    eval (Leaf a) = vector a
-    eval (Graft a b) = bilinear graft (eval a) (eval b)
-    eval (Concat as) = product $ map eval as
-
-    syn [PT a []] = Leaf a
-    syn [PT a bs] = Graft (syn bs) (Leaf a)
-    syn ts = Concat $ map (syn . (:[])) ts
-
-    compose _ [] (Leaf y) = Just $ Leaf y
-    compose _ [y] (Leaf _) = Just y
-    compose _ _ (Leaf _) = Nothing
-    compose _ _ (Concat []) = Just $ Concat []
-    compose x ops obj
-        | countSubtrees x obj == length ops =
-            Just $ case obj of
-                Graft a b ->
-                    Graft
-                        (fromJust $ compose x (take (countSubtrees x a) ops) a)
-                        (fromJust $ compose x (drop (countSubtrees x a) ops) b)
-                Concat as -> Concat $ map (\(a, ops_a) -> fromJust $ compose x ops_a a) $ spreadOps ops as
-        | otherwise = Nothing
-      where
-        spreadOps os [a] = [(a, os)]
-        spreadOps os (a : as) = (a, take (countSubtrees x a) os) : (spreadOps (drop (countSubtrees x a) os) as)
-
-        countSubtrees x y = if x == y then 1 else case y of
-            Graft a b -> countSubtrees x a + countSubtrees x b
-            Concat as -> sum $ map (countSubtrees x) as
-            _ -> 0
-
-symCompose
-    :: ( IsSyntacticTree st
-       , Eq st
-       , Graded st
+compose
+    :: ( Eq a
+       , Graded a
        )
-    => st
-    -> [st]
-    -> st
-    -> Vector Integer st
-symCompose x ops obj =
+    => SyntacticTree a
+    -> [SyntacticTree a]
+    -> SyntacticTree a
+    -> Maybe (SyntacticTree a)
+compose _ [] (Leaf y) = Just $ Leaf y
+compose _ [y] (Leaf _) = Just y
+compose _ _ (Leaf _) = Nothing
+compose _ _ (Node op []) = Just $ Node op []
+compose x ops (Node op as) = Just $ Node op $ map (\(a, ops_a) -> fromJust $ compose x ops_a a) $ spreadOps ops as
+  where
+    spreadOps os [a] = [(a, os)]
+    spreadOps os (a : as) = (a, take (countSubtrees x a) os) : (spreadOps (drop (countSubtrees x a) os) as)
+
+    countSubtrees x y =
+        if x == y
+            then 1
+            else case y of
+                Leaf _ -> 0
+                Node _ as -> sum $ map (countSubtrees x) as
+                _ -> 0
+
+symmetricCompose
+    :: ( IsVector a
+       , Eq (VectorScalar a)
+       , Num (VectorScalar a)
+       , Eq a
+       , Graded a
+       )
+    => SyntacticTree a
+    -> [SyntacticTree a]
+    -> SyntacticTree a
+    -> Vector (VectorScalar (SyntacticTree a)) (SyntacticTree a)
+symmetricCompose x ops obj =
     mconcat
         $ map
             ( \perm_ops -> case compose x perm_ops obj of
@@ -373,25 +384,21 @@ symCompose x ops obj =
             )
         $ permutations ops
 
-substitution
-    :: ( IsSyntacticTree st
-       , Eq st
-       , Graded st
-       , Eq (EvaluatesTo st)
-       , Graded (EvaluatesTo st)
-       )
-    => st
-    -> [EvaluatesTo st]
-    -> EvaluatesTo st
-    -> Vector Integer (EvaluatesTo st)
-substitution x gens obj = linear eval $ symCompose x (map syn gens) $ syn obj
+class (IsVector a) => HasSyntacticTree a where
+    syn :: a -> Vector (VectorScalar a) (SyntacticTree a)
 
-class Substitutable t where
-    type Generator t
+eval :: SyntacticTree a -> Vector (VectorScalar a) (VectorBasis a)
+eval (Leaf a) = vector a
+eval (Node op as) = linear (func op) $ product $ map eval as
 
-    substitute :: Generator t -> t -> t -> Vector Integer t
+graftOp :: (IsVector a, Graftable a) => Operation a
+graftOp = Op "graft" "$\\curvearrowright$" 2 $
+    \ops -> case ops of
+        [x, y] -> graft x y
+        _ -> error "graftOp: arity"
 
-instance (Eq d, Graded d) => Substitutable [PlanarTree d] where 
-    type Generator [PlanarTree d] = d
+concatOp :: (IsVector a, Monoid a) => Operation a
+concatOp = Op "concat" "$\\cdot$" (-1) $
+    \ops -> vector $ mconcat ops
 
-    substitute x gens obj = substitution (Leaf x) (map (: []) gens) obj
+
